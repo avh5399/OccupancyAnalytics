63efdcc1e52b4dff1953e1eedbcc5a1f9c8a25c5
AribHyder <52585619+AribHyder@users.noreply.github.com>
2020-12-08 22:57:24 -0500

    update parameters
    update parameters

--- pirtest.py
+++ pirtest.py
@@ -5,7 +5,13 @@ from writeFromRPIToDb import post
 
 start_time = time.time()
 counter = 0
+roomID = '1'
 occupancyValue = '0'
+server = 'room-occupancy.database.windows.net'
+database = 'RoomsDB'
+username = 'user'
+password = 'Capstone20'
+driver= '{ODBC Driver 17 for SQL Server}'
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -24,7 +30,7 @@ while True:
     elapsed_time = current_time - start_time
     
     if (elasped_time > 60):
-        post(occupancyValue)
+        post(occupancyValue, driver, server, database, username, password)
         occupancyValue = '0'
         counter = 0
         start_time = time.time()
--- writeFromRPIToDB.py
+++ writeFromRPIToDB.py
@@ -3,17 +3,8 @@ import pyodbc
 #if that fails and you're on macOS, try brew install unixodbc and then pip3 install pyodbc
 #still errors? Try this https://github.com/mkleehammer/pyodbc/issues/717 and https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/install-microsoft-odbc-driver-sql-server-macos?view=sql-server-ver15
 
-roomID = '1'
-occupancyValue = '0'
 
-server = 'room-occupancy.database.windows.net'
-database = 'RoomsDB'
-username = 'user'
-password = 'Capstone20'
-driver= '{ODBC Driver 17 for SQL Server}'
-
-
-def post(occupancyValue):
+def post(occupancyValue, driver, server, database, username, password):
     with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT * FROM rooms")
